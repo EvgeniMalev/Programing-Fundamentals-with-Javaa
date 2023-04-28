3caca96a6df118bd94b2b2292805f182f2a9e0ba
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2023-04-28 22:36:53 +0300

    Create Chicken.java

---
+++ Objects and classes/Chickencoop/Chicken.java
@@ -0,0 +1,43 @@
+
+  public class Chicken {
+   private String typeList;
+   private String breed;
+   private String quantity;
+   
+  public Chciken(String typeList, String breed, String quantity) {
+   this.typeList = typeList;
+   this.breed = breed;
+   this.quantity = quantity;
+  }
+  
+  public String getTypeList(String typeList){
+      return typeList;
+  }
+
+  public void setTypeList(String typeList) {
+    this.typeList = typeList;
+  }
+  
+    public String getbreed(String breed){
+      return breed;
+  }
+
+  public void setbreed(String breed) {
+    this.breed = breed;
+  }
+  
+      public String getquantity(String quantity){
+      return breed;
+  }
+
+  public void setquantity(String quantity) {
+    this.breed = quantity;
+  }
+  
+  public string getbreed() {
+     return breed;
+  }
+
+  public Object getTypeList() {
+     return typeList;
+  }	 
