8d12345fe70e29dd1c2331dd435c7f3afaab49a4
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-07-16 23:25:38 +0300

    Create diagonalssum.java

---
+++ Sirma/Multidimensional arrays/diagonalssum.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.nextLine(); 
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] elements = scanner.nextLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = Integer.parseInt(elements[j]);
+            }
+        }
+
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+        int totalsum = 0;
+        
+        System.out.print("First diagonal: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(matrix[i][i] + " ");
+            primaryDiagonalSum += matrix[i][i];
+        }
+        System.out.println();
+
+        System.out.print("Second diagonal: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(matrix[i][n - 1 - i] + " ");
+            secondaryDiagonalSum += matrix[i][n - 1 - i];
+        }
+        System.out.println();
+        
+        
+
+        totalsum = secondaryDiagonalSum + primaryDiagonalSum;
+
+        System.out.println("Sum: " + totalsum);
+
+        scanner.close();
+    }
+}
